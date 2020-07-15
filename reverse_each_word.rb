8f244d0a808cac0ba7ca8449d8a97ad6acbeb6a9
Lauren Anderson <laurenaanderson95@gmail.com>
2020-07-15 01:51:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   while sentence.split 
-    sentence.reverse.each_with_index do |word|
+    sentence.each_with_index do |word|
       "#{word}".reverse
     end
   end

cfdf0f598f5b3e10e2b74498e3a9a6c75b88ceb3
Lauren Anderson <laurenaanderson95@gmail.com>
2020-07-15 02:24:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 require 'pry'
 
 def reverse_each_word(sentence)
- sentence1 = sentence.splitbinding.pry 
+ sentence1 = sentence.split
+ binding.pry 
     sentence1.each do |word|
       
       word.reverse
